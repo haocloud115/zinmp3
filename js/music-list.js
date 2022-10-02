1bc34c434a16f54d716e5a284c924393ae825dfa
haocloud115 <wormvirrus@gmail.com>
2022-10-02 09:09:46 +0700

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,129 @@
+let allMusic = [
+    {
+        name: "Why?",
+        artist: "Bazzi",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Tones And I",
+        artist: "Dance Monkey",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "double take",
+        artist: "dhruv",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Attention",
+        artist: "Charlie Puth",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Closer",
+        artist: "Chainsmokers ft Halsey",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "See You Again",
+        artist: "Wiz Khalifa ft Charlie Puth",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Let Her Go",
+        artist: "Passenger",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Faded",
+        artist: "Alan Walker",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Demons",
+        artist: "Imagine Dragons",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Heat Waves",
+        artist: "Glass Animals",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Tại Vì Sao",
+        artist: "RPT MCK",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "If You Said So",
+        artist: "Coldzy ft Wxrdie 2pillz",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Cô Nàng Khác Người",
+        artist: "Pixel Neko ft Nger MCK Wxrdie",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Vua Chơi Trên Mặt Trăng",
+        artist: "Low G ft Chí",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Thích Quá Rùi Nà",
+        artist: "tlinh ft Trung Trần (prod by Pacman)",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Gps (Remix)",
+        artist: "Nidebig (feat. Ybapurr, Binqang, Hs.lilyafuu)",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Blue Tequila",
+        artist: "Táo",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name: "Chán Gái 707",
+        artist: "Low G",
+        img: "music-18",
+        src: "music-18"
+    },
+    {
+        name: "BERLIN",
+        artist: "Khoi Vu",
+        img: "music-19",
+        src: "music-19"
+    },
+    {
+        name: "Nghe Như Tình Yêu",
+        artist: "HIEUTHUHAI (prod by Kewtiie)",
+        img: "music-19",
+        src: "music-19"
+    },
+
+
+
+
+
+
+
+];
